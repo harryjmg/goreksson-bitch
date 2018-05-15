4997b7bd7cf0bc4459801c7209eb16cef195d6e6
Harry du Rails Heroes Course <harry.jmg@gmail.com>
2018-05-15 14:30:05 +0000

    tmtc

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,9 +1,16 @@
-
 require 'facebook/messenger'
+
 include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+    if message.text.include? "Bonjour"
+       message.reply(text: "Wesh maggl") 
+   elsif message.text.include? "Bonne nuit"
+        message.reply(text: "Dors bien fdp") 
+    else
+        message.reply(text: 'Reponse par defaut')
+    end
+  
 end
